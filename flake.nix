c423988355a87b13b82db09e779cb4c9decc026c
4a-42 <22332960+4a-42@users.noreply.github.com>
2025-09-25 21:02:00 -0500

    shelve windows support for now

--- flake.nix
+++ flake.nix
@@ -35,11 +35,15 @@
           localSystem = system;
           crossSystem = pkgs.lib.systems.examples.musl64;
         };
-        pkgs_windows = import nixpkgs {
-          inherit overlays;
-          localSystem = system;
-          crossSystem = pkgs.lib.systems.examples.mingwW64;
-        };
+        # https://github.com/NixOS/nixpkgs/tree/master/pkgs/os-specific/windows/msvcSdk
+        # add windows.sdk to native build inputs
+        # pkgs_windows = import nixpkgs {
+        #   inherit overlays;
+        #   localSystem = system;
+        #   crossSystem.config = "x86_64-pc-windows-msvc";
+        #   config.microsoftVisualStudioLicenseAccepted = true;
+        #   config.allowUnfree = true;
+        # };
 
         rustToolchain = fenix.packages.${system}.fromToolchainFile {
           file = ./rust-toolchain.toml;
@@ -53,13 +57,14 @@
 
         src = ./.;
 
-        nativeBuildInputs = with pkgs; [
+        nativeBuildInputs = [
           rustToolchain
           # rustPlatform.bindgenHook
-          rust-bindgen
-          just
-          nushell
-          p7zip
+          pkgs.rust-bindgen
+          pkgs.just
+          pkgs.nushell
+          pkgs.p7zip
+          # pkgs_windows.windows.sdk
         ];
 
         commonEnvVars = {
@@ -76,24 +81,25 @@
             "--target=x86_64-unknown-linux-musl"
             "-I${pkgs_amd64.libclang.lib}/lib/clang/${pkgs.lib.versions.major (pkgs.lib.getVersion pkgs_amd64.clang)}/include"
           ];
-          BINDGEN_EXTRA_CLANG_ARGS_x86_64_pc_windows_gnu = builtins.concatStringsSep " " [
-            "--target=x86_64-pc-windows-gnu"
-            "-I${pkgs_windows.libclang.lib}/lib/clang/${pkgs.lib.versions.major (pkgs.lib.getVersion pkgs_windows.clang)}/include"
-          ];
+          # BINDGEN_EXTRA_CLANG_ARGS_x86_64_pc_windows_msvc = builtins.concatStringsSep " " [
+          #   "--target=x86_64-pc-windows-msvc"
+          #   "-I${pkgs_windows.libclang.lib}/lib/clang/${pkgs.lib.versions.major (pkgs.lib.getVersion pkgs_windows.clang)}/include"
+          # ];
           CARGO_PROFILE_RELEASE_BUILD_OVERRIDE_DEBUG = true;
           CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER = "${pkgs_aarch64.stdenv.cc}/bin/${pkgs_aarch64.stdenv.cc.targetPrefix}gcc";
           CARGO_TARGET_ARMV7_UNKNOWN_LINUX_MUSLEABIHF_LINKER = "${pkgs_arm.stdenv.cc}/bin/${pkgs_arm.stdenv.cc.targetPrefix}gcc";
           CARGO_TARGET_X86_64_UNKNOWN_LINUX_MUSL_LINKER = "${pkgs_amd64.stdenv.cc}/bin/${pkgs_amd64.stdenv.cc.targetPrefix}gcc";
-          CARGO_TARGET_X86_64_PC_WINDOWS_GNU_LINKER = "${pkgs_windows.stdenv.cc}/bin/${pkgs_windows.stdenv.cc.targetPrefix}gcc";
+          # CARGO_TARGET_X86_64_PC_WINDOWS_MSVC_LINKER = "${pkgs_windows.stdenv.cc}/bin/${pkgs_windows.stdenv.cc.targetPrefix}gcc";
           CC_ENABLE_DEBUG_OUTPUT = true;
           CC_aarch64_unknown_linux_musl = "${pkgs_aarch64.stdenv.cc}/bin/${pkgs_aarch64.stdenv.cc.targetPrefix}gcc";
           CC_armv7_unknown_linux_musl = "${pkgs_arm.stdenv.cc}/bin/${pkgs_arm.stdenv.cc.targetPrefix}gcc";
           CC_x86_64_unknown_linux_musl = "${pkgs_amd64.stdenv.cc}/bin/${pkgs_amd64.stdenv.cc.targetPrefix}gcc";
-          CC_x86_64_pc_windows_gnu = "${pkgs_windows.stdenv.cc}/bin/${pkgs_windows.stdenv.cc.targetPrefix}gcc";
+          # CC_x86_64_pc_windows_msvc = "${pkgs_windows.stdenv.cc}/bin/${pkgs_windows.stdenv.cc.targetPrefix}gcc";
           CRATE_CC_NO_DEFAULTS = true;
           # Bindgen seems to want this to point to host's libclang, even when cross-compiling
           LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
           RUST_BACKTRACE = 1;
+          # NIXPKGS_ALLOW_UNFREE = 1;
         };
 
         commonArgs =
@@ -117,13 +123,13 @@
         craneLib-aarch64 = (crane.mkLib pkgs_aarch64).overrideToolchain rustToolchain;
         craneLib-arm = (crane.mkLib pkgs_arm).overrideToolchain rustToolchain;
         craneLib-amd64 = (crane.mkLib pkgs_amd64).overrideToolchain rustToolchain;
-        craneLib-windows = (crane.mkLib pkgs_windows).overrideToolchain rustToolchain;
+        # craneLib-windows = (crane.mkLib pkgs_windows).overrideToolchain rustToolchain;
 
         cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {nativeBuildInputs = [pkgs.rustPlatform.bindgenHook];});
         cargoArtifacts-aarch64 = craneLib-aarch64.buildDepsOnly (commonArgs // {CARGO_BUILD_TARGET = "aarch64-unknown-linux-musl";});
         cargoArtifacts-arm = craneLib-arm.buildDepsOnly (commonArgs // {CARGO_BUILD_TARGET = "armv7-unknown-linux-musleabihf";});
         cargoArtifacts-amd64 = craneLib-amd64.buildDepsOnly (commonArgs // {CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";});
-        cargoArtifacts-windows = craneLib-windows.buildDepsOnly (commonArgs // {CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";});
+        # cargoArtifacts-windows = craneLib-windows.buildDepsOnly (commonArgs // {CARGO_BUILD_TARGET = "x86_64-pc-windows-msvc";});
 
         my-crate = craneLib.buildPackage (
           commonArgs // {inherit cargoArtifacts;}
@@ -149,20 +155,20 @@
             CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
           }
         );
-        my-crate-windows = craneLib-windows.buildPackage (
-          commonArgs
-          // {
-            inherit cargoArtifacts-windows;
-            CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
-          }
-        );
+        # my-crate-windows = craneLib-windows.buildPackage (
+        #   commonArgs
+        #   // {
+        #     inherit cargoArtifacts-windows;
+        #     CARGO_BUILD_TARGET = "x86_64-pc-windows-msvc";
+        #   }
+        # );
       in {
         packages = {
           default = my-crate;
           cross-aarch64 = my-crate-aarch64;
           cross-arm = my-crate-arm;
           cross-amd64 = my-crate-amd64;
-          cross-windows = my-crate-windows;
+          # cross-windows = my-crate-windows;
         };
         devShells = {
           default = craneLib.devShell {packages = nativeBuildInputs ++ [pkgs.rustPlatform.bindgenHook];};
@@ -181,11 +187,11 @@
               packages = nativeBuildInputs;
             }
             // commonEnvVars);
-          cross-windows = craneLib-windows.devShell ({
-              CARGO_BUILD_TARGET = "x86_64-pc-windows-gnu";
-              packages = nativeBuildInputs;
-            }
-            // commonEnvVars);
+          # cross-windows = craneLib-windows.devShell ({
+          #     CARGO_BUILD_TARGET = "x86_64-pc-windows-msvc";
+          #     packages = nativeBuildInputs;
+          #   }
+          #   // commonEnvVars);
         };
       }
     );
